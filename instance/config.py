228706074f0603978df2d1faaa71b5d511eefe5e
Niko Fink <nfink95@gmail.com>
2016-10-01 20:17:03 -0400

    import of files from sftp://sensordc@blizzard.cs.uwaterloo.ca/home/sensordc/webikewebsiteprod

---
+++ instance/config.py
@@ -0,0 +1,8 @@
+DEBUG = True
+HOSTNAME = "blizzard.cs.uwaterloo.ca"
+PORT = 5000
+DB_HOST = "typhoon.cs.uwaterloo.ca"
+DB_PORT = 3306
+DB_NAME = "webike"
+DB_CREDENTIALS_PATH = "/u2/tcarpenter/webikewebsite/blizzard/dbcredentials.txt"
+ASSETS_PATH = "/~sensordc/bootstrap"
